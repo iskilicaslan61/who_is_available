d53545937b7bee610a620fbe69410e6d2e800710
Ismail <iskilicaslan61@gmail.com>
2025-05-20 12:34:56 +0200

    code is updated

--- app.py
+++ app.py
@@ -18,9 +18,7 @@ people = [
 def home():
     return render_template('index.html', people=people)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  
-    app.run(host="0.0.0.0", port=port)  
+ 
     
 @app.route('/toggle_availability/<int:person_id>', methods=['POST'])
 def toggle_availability(person_id):
@@ -29,5 +27,8 @@ def toggle_availability(person_id):
         person["available"] = not person["available"]
     return jsonify({"status": "success", "new_status": person["available"]})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host="0.0.0.0", port=port) 
+    
