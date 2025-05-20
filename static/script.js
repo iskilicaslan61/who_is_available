1edb2c3d4ef516d4e82137bd7a3a95d68063fc86
Ismail <iskilicaslan61@gmail.com>
2025-05-20 13:07:47 +0200

    code is updated

--- static/script.js
+++ static/script.js
@@ -1,21 +1,16 @@
-<script>
+document.addEventListener('DOMContentLoaded', function() {
         function toggleAvailability(personId) {
-            fetch(`/toggle_availability/${personId}`, {
-                method: 'POST',
-            })
-            .then(response => response.json())
-            .then(data => {
-                let statusSpan = document.getElementById(`status-${personId}`);
-                if (data.new_status) {
-                    statusSpan.className = 'available';
-                    statusSpan.textContent = 'Available';
-                } else {
-                    statusSpan.className = 'not-available';
-                    statusSpan.textContent = 'Not Available';
-                }
-            })
-            .catch(error => {
-                console.error("Error toggling availability:", error);
-            })
+            fetch(`/toggle_availability/${personId}`)
+                .then(response => response.json())
+                .then(data => {
+                    const statusSpan = document.getElementById(`status-${personId}`);
+                    if (data.available) {
+                        statusSpan.textContent = 'Available';
+                        statusSpan.className = 'available';
+                    } else {
+                        statusSpan.textContent = 'Not Available';
+                        statusSpan.className = 'not-available';
+                    }
+                });
         }
-    </script>
+    });
